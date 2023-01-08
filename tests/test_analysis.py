98b97776fad0a64bc4a774af4b4a699178545b43
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-01-08 17:14:10 +0000

    tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,6 +1,20 @@
+import pytest
 import pandas as pd
+from pycta.analysis import Analysis
 
-def test_analysis(resource_dir):
+@pytest.fixture
+def analysis(resource_dir):
     prices = pd.read_csv(resource_dir / "prices.csv", index_col=0, parse_dates=True, header=0)
-    print(prices)
-    assert False
+    # we need only one time series
+    nav = prices["B"]
+    return Analysis(nav)
+
+
+def test_std(analysis):
+    assert analysis.std["2013-02-03"] == pytest.approx(0.004341629905864831)
+
+
+def test_monthlytable(analysis):
+    assert analysis.monthlytable["STDev"].loc[2015] == "18.89%"
+
+    
