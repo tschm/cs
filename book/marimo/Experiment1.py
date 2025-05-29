dae6e76f17a7afac7bffed0696f4de952bc1608d
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-05-29 08:12:16 +0400

    fixing the first notebook

--- book/marimo/Experiment1.py
+++ book/marimo/Experiment1.py
@@ -6,11 +6,7 @@ app = marimo.App()
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-        # CTA 1.0
-        """
-    )
+    mo.md(r"""# CTA 1.0""")
     return
 
 
@@ -18,21 +14,33 @@ def _(mo):
 def _():
     import warnings
 
+    import marimo as mo
     import pandas as pd
     import numpy as np
+    import plotly.io as pio
 
-    # from cvx.simulator import Builder
-    from cvx.simulator import interpolate
-    from cvx.simulator import Portfolio
+    # Ensure Plotly works with Marimo
+    pio.renderers.default = "plotly_mimetype"
 
+    # Suppress noisy warnings
     warnings.simplefilter(action="ignore", category=FutureWarning)
-    return Portfolio, interpolate, np, pd
+
+    # Optional: import simulation modules
+    from cvx.simulator import interpolate, Portfolio
+
+    return Portfolio, interpolate, mo, np, pd, pio
 
 
 @app.cell
 def _(interpolate, pd):
+    from pathlib import Path
+
+    path = Path(__file__).parent
+
     # Load prices
-    prices = pd.read_csv("data/Prices_hashed.csv", index_col=0, parse_dates=True)
+    prices = pd.read_csv(
+        path / "data" / "Prices_hashed.csv", index_col=0, parse_dates=True
+    )
 
     # interpolate the prices
     prices = prices.apply(interpolate)
@@ -51,14 +59,12 @@ def _(np):
 
 
 @app.cell
-def _():
-    from ipywidgets import Label, HBox, VBox, IntSlider
+def _(mo):
+    fast = mo.ui.slider(4, 192, step=4, value=32, label="Fast moving average")
+    slow = mo.ui.slider(4, 192, step=4, value=96, label="Slow moving average")
 
-    fast = IntSlider(min=4, max=192, step=4, value=32)
-    slow = IntSlider(min=4, max=192, step=4, value=96)
-    left_box = VBox([Label("Fast Moving Average"), Label("Slow Moving Average")])
-    right_box = VBox([fast, slow])
-    HBox([left_box, right_box])
+    mo.vstack([fast, slow])
+
     return fast, slow
 
 
@@ -89,12 +95,12 @@ def _(Portfolio, pos, prices):
 def _(mo):
     mo.md(
         r"""
-        Results do not look terrible but...
-        * No concept of risk integrated.
-        * The size of each bet is constant regardless of the underlying asset.
-        * The system lost its mojo in 2009 and has never really recovered.
-        * The sign function is very expensive to trade as position changes are too extreme.
-        """
+    Results do not look terrible but...
+    * No concept of risk integrated.
+    * The size of each bet is constant regardless of the underlying asset.
+    * The system lost its mojo in 2009 and has never really recovered.
+    * The sign function is very expensive to trade as position changes are too extreme.
+    """
     )
     return
 
@@ -102,24 +108,21 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(
-        r"""
-        Such fundamental flaws are not addressed by **parameter-hacking** or **pimp-my-trading-system** steps (remove the worst performing assets, insane quantity of stop-loss limits, ...)
-
-        """
+        r"""Such fundamental flaws are not addressed by **parameter-hacking** or **pimp-my-trading-system** steps (remove the worst performing assets, insane quantity of stop-loss limits, ...)"""
     )
     return
 
 
 @app.cell
 def _(portfolio):
-    portfolio.snapshot()
+    fig = portfolio.snapshot()
+    fig
     return
 
 
 @app.cell
-def _(pd, portfolio):
+def _(pd):
     pd.set_option("display.precision", 2)
-    portfolio.sharpe()
     return
 
 
@@ -127,19 +130,25 @@ def _(pd, portfolio):
 def _(mo):
     mo.md(
         r"""
-        cvxSimulator can construct portfolio objects. Those objects will expose functionality and attributes supporting all analytics.
-        There are two types of portfolio -- EquityPortfolio and FuturesPortfolio. We start with the FuturesPortfolio. The most simple use-case
-        is when we have computed all desirec cash-positions
-        """
+    cvxSimulator can construct portfolio objects. Those objects will expose functionality and attributes supporting all analytics.
+    There are two types of portfolio -- EquityPortfolio and FuturesPortfolio. We start with the FuturesPortfolio. The most simple use-case
+    is when we have computed all desirec cash-positions
+    """
     )
     return
 
 
 @app.cell
-def _():
-    import marimo as mo
+def _(pio):
+    for renderer in pio.renderers:
+        print(renderer)
 
-    return (mo,)
+    return
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
